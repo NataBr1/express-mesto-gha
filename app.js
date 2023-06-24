57eddb5b5d52d7d6fec9400a9f6d2d313c44830a
Natalya Bryzgalova <Natasha.Brizgalova@yandex.rugit config --global user.email Natasha.Brizgalova@yandex.ru>
2023-06-24 23:33:09 +0300

    Исправление ошибок

--- app.js
+++ app.js
@@ -21,14 +21,14 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-router.post('/signin', celebrate({
+app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
 }), login);
 
-router.post('/signup', celebrate({
+app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
