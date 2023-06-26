ad349a1edc3ac9e0c52eced4bc12ff33f06eea92
Natalya Bryzgalova <Natasha.Brizgalova@yandex.rugit config --global user.email Natasha.Brizgalova@yandex.ru>
2023-06-26 12:41:30 +0300

    Исправление ошибок

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,12 +2,15 @@ const jwt = require('jsonwebtoken');
 const AuthError = require('../errors/auth_err');
 
 const auth = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new AuthError('Необходима авторизация');
+  }
   const token = req.cookies.jwt;
   let payload;
   try {
     payload = jwt.verify(token, 'SECRET');
-    // eslint-disable-next-line no-console
-    console.log(payload);
   } catch (err) {
     throw new AuthError('Необходимо авторизоваться');
   }
